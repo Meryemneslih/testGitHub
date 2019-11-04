a2a9f55f9a0cf3430f45356659f2d53226d0d55f
Meryemneslih <mnesli@gmail.com>
2019-11-04 13:33:59 -0500

    home Work anagram

---
+++ javaClass/src/day21/homeWorkHashMapAnagram.java
@@ -0,0 +1,58 @@
+package day21;
+
+import java.util.Arrays;
+
+public class homeWorkHashMapAnagram {
+    public static void main(String[] args) {
+//Given to strings s and t Write a java program to determine if t is an anagram of s
+        //input s:anagram   , t=nagaram
+
+        String s = "anagram";
+        String  t= "nagaram";
+
+
+//        char c = s.charAt();
+//        System.out.println(c);
+
+        char[] c = s.toCharArray();
+        System.out.println(c);
+        Arrays.sort(c);
+        System.out.println(c);
+
+        char[] d = t.toCharArray();
+        System.out.println(d);
+        Arrays.sort(d);
+        System.out.println(d);
+
+        boolean b=true;
+        for(int i=0;i<d.length;i++) {
+            if (c[i] == d[i]) {
+                b=true;
+                System.out.println(b);
+                break;
+
+            }
+
+//            boolean isEqual = true;
+//            for (int i = 0; i < arrayA.length; i++) {
+//                // 1. 5 != 5 => false
+//                // 2. 8 != 6 => true
+//                if (arrayA[i] != arrayB[i]) {
+//                    isEqual = false;
+//                    break;
+//                }
+
+
+
+        }
+
+//        for (int i=0;i<s.length();i++) {
+//            System.out.println(s.charAt(i));
+//        }
+//        for (int i=0;i<t.length();i++) {
+//            System.out.println(t.charAt(i));
+//        }
+
+
+    }
+}
